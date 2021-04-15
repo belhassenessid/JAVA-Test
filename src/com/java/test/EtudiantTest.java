4ba3e1ab0bdce4c632f3edcda6af2e42d3ee3a9e
belhassen essid <essidbelhassan@gmail.com>
2021-04-15 19:24:38 +0100

    final commit

--- src/com/java/test/EtudiantTest.java
+++ src/com/java/test/EtudiantTest.java
@@ -7,35 +7,41 @@ import junit.framework.*;
 
 public class EtudiantTest extends TestCase {
 	
-	Etudiant tous_les_etudiants[] = new Etudiant[3]; //liste de tous les etudiants
-	ListeEtudiants CS =new ListeEtudiants(); //liste des étudiants du filière CS
-	ListeEtudiants IRS =new ListeEtudiants(); //liste des étudiants du filière IRS
+	Etudiant tous_les_etudiants[] = new Etudiant[3]; //Créer liste de tous les etudiants
+	ListeEtudiants CS =new ListeEtudiants(); //Creér liste des étudiants du filière CS
+	ListeEtudiants IRS =new ListeEtudiants(); //Créer liste des étudiants du filière IRS
 	
 	
 	public void test() throws Exception {	
 		
 	// TEST Constructor	
 	// tester que les étudiants sont construits et qu'ils sont bien ajoutés à l'ensemble des étudiants
-	tous_les_etudiants[0] = new Etudiant("Ali","Ben Ali","Python");
-	tous_les_etudiants[1] = new Etudiant("Asma","Farhat","Python");
-	tous_les_etudiants[2] = new Etudiant("Med","Ayari","Maths");
+	tous_les_etudiants[0] = new Etudiant("Ali","Ben Ali","CS");
+	tous_les_etudiants[1] = new Etudiant("Asma","Farhat","CS");
+	tous_les_etudiants[2] = new Etudiant("Med","Ayari","IRS");
+	
 	
 	assertNotNull(tous_les_etudiants[0]);
 	assertNotNull(tous_les_etudiants[1]);
 	assertNotNull(tous_les_etudiants[2]); 
 	
+	
+	
 	// TEST d'ajout
-	// tester que les étudiants peuvent etre bien ajoutés à leurs listes de filière (Python/Maths)
+	// tester que les étudiants peuvent etre bien ajoutés à leurs listes de filière (CS/IRS)
 	CS.ajouterEtudiant(tous_les_etudiants[0]);
 	CS.ajouterEtudiant(tous_les_etudiants[1]);
 	IRS.ajouterEtudiant(tous_les_etudiants[2]);
 	
-	assertEquals(CS.getEtudiantAt(0).getFiliere(),"Python");
-	assertEquals(IRS.getEtudiantAt(0).getFiliere(),"Maths");
+	
+	assertEquals(CS.getEtudiantAt(0).getFiliere(),"CS");
+	assertEquals(IRS.getEtudiantAt(0).getFiliere(),"IRS");
+	
 	
 	// TEST de suppression
 	CS.supprimerEtudiant(tous_les_etudiants[1]);
-	assertEquals(CS.NombreEtudiants(),1); //si la suppression est réussie, le nombre d'étudiants Python doit être 2-1==1
+	//si la suppression est réussie, le nombre d'étudiants CS doit être 2-1==1
+	assertEquals(CS.NombreEtudiants(),1); 
 	
 	// Test update && Saisie de notes
 	// tester que nous pouvons modifier les étudiants en leur ajoutant de nouvelles notes
@@ -46,8 +52,8 @@ public class EtudiantTest extends TestCase {
 	assertEquals(IRS.getEtudiantAt(0).MoyGen() , (13.50+19.50+10.50+08.75)/4);
 	
 	// Test Affichage des moyennes des matieres, statistiques -calcule moyenne gen- et quelques autres options -id, non et prenom
-	assertEquals(CS.getEtudiantAt(0).toString() , "ID:1, Ali Ben Ali / Filiere : Python / Python= 15.5 Test=7.25 UML=16.75 Maths=18.25 |==> MoyGen=14.4375");
-	
+	assertEquals(CS.getEtudiantAt(0).toString() , "ID:1, Ali Ben Ali / Filiere : CS / Python= 15.5 Test=7.25 UML=16.75 Maths=18.25 |==> MoyGen=14.4375");
+	System.out.println(IRS.getEtudiantAt(0).toString());
 
 	
 	}
